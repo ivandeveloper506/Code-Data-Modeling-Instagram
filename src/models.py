15681a91612d5418192d10715f4b97ca3762a85b
ivandeveloper506 <ivanfonsecacr@gmail.com>
2021-04-03 03:40:59 +0000

    Code-Data-Modeling-Instagram   Initial Version.

--- src/models.py
+++ src/models.py
@@ -1,6 +1,15 @@
+#*******************************************************************************#
+# Fecha Creación:  02 Abril 2021.                                               #
+# Autor:           Iván Fonseca Castro                                          #
+#                                                                               #
+# Descripción:     Modelo para las entidades que componen el Proyecto del       #
+#                  Instagram                                                    #
+#*******************************************************************************#
+
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Date, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,24 +17,62 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# Definición de Tipo de Dato Personalizado para identificar el tipo de Media
+class MediaTypeEnum(enum.Enum):
+    Normal = 1
+    Clarendon = 2
+    Gingham = 3
+    Moon = 4
+    Lark = 5
+    Reyes = 6
+    Juno = 7
+
+# Definición de Entidades Principales
+# [user] : Esta entidad permitirá almacenar los usuarios.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    first_surname = Column(String(100), nullable=False)
+    second_surname = Column(String(100))
+    email = Column(String(250), unique=True)
+    user_name = Column(String(50), unique=True)
+    password = Column(String(50))
+    user_image = Column(String(2000))
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    user_fo_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_description = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    media_type = Column(Enum(MediaTypeEnum), nullable=False)
+    url = Column(String(1000), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(1000), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+ 
     def to_dict(self):
         return {}
 
